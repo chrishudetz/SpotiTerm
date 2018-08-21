19a58109fe9267ac72af64b769cb0a31b51774bd
Dextroz <dextrozmlg@gmail.com>
2018-08-21 15:37:50 +0100

    Edited docstring, added err

--- functions/option2.py
+++ functions/option2.py
@@ -9,8 +9,8 @@ try:
     from .authentication import auth
     # For exception in procedure.
     import spotipy.oauth2
-except ImportError:
-    print("Failed to import modules for option2.py")
+except ImportError as err:
+    print(f"Failed to import modules for option2.py: {err}")
 
 """
 Option 2. Lookup an Artists top 10 tracks. Specified by user.
@@ -18,6 +18,7 @@ Args:
     sp: auth() from authentication.py; Returns sp object.
 Exceptions:
     spotipy.client.SpotifyException
+    spotipy.oauth2.SpotifyOauthError
 """
 
 
@@ -25,14 +26,15 @@ def artist_top_10(sp):
     try:
         artist_name = input(info(green("What is the artist name: ")))
         # resp for searching artist name. Get top artist tracks.
-        resp = sp.search(q=artist_name, limit=11)
-        for item, track in enumerate(resp["tracks"]["items"]):
-            print(green(" ""{} {}").format(item, track["name"]))
+        resp = sp.search(q=artist_name, limit=10)
+        # int: 1 is passed to start from 1 instead of 0.
+        for item, track in enumerate(resp["tracks"]["items"], 1):
+            print(green(f" {item} {track['name']}"))
         # Sleep to observe above print.
         sleep(8)
-    except spotipy.client.SpotifyException:
-        print(green("Top 10 track lookup failed."))
+    except spotipy.client.SpotifyException as err:
+        print(green(f"Top 10 track lookup failed: {err}"))
         sleep(2)
-    except spotipy.oauth2.SpotifyOauthError:
-        print(green("Bad request. Check Client ID and Client S."))
+    except spotipy.oauth2.SpotifyOauthError as err:
+        print(green(f"Bad request. Check Client ID and Client S: {err}"))
         sleep(2)
